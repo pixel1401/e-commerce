3f1f8d8b644c123a443f1c1252c442175b42103d
pixel1401 <pixel1401@gmail.com>
2022-12-27 20:51:32 +0500

    log out , isSignIn

--- src/Components/Home.tsx
+++ src/Components/Home.tsx
@@ -2,11 +2,38 @@ import { NavBar } from "./Navbar";
 import { Products } from "./Products";
 import Container from 'react-bootstrap/Container';
 import Col from "react-bootstrap/esm/Col";
+import { useEffect, useState } from "react";
+import { auth, firebaseCollectionName, fs } from "../config/Config";
+import { IUsers } from "../Models/IUsers";
 
 export function Home () {
+
+    function GetCurrentUser  ()  {
+        const [user, setUser] = useState<IUsers | null >(null);
+        useEffect(()=> {
+            auth.onAuthStateChanged(user=> {
+                if(user) {
+                    fs.collection(firebaseCollectionName.users).doc(user.uid).get()
+                    .then((snapshot) => {
+                        setUser(snapshot!.data() as IUsers);
+                    })
+                }else {
+                    setUser(null);
+                }
+            })
+        }, []);
+        return user;
+    }
+
+
+    const user = GetCurrentUser();
+    console.log(user);
+    
+    
+
     return (
         <>
-            <NavBar/>
+            <NavBar user={user} />
             <Products/>
         </>
     )
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -5,30 +5,73 @@ import { Link } from 'react-router-dom';
 import { RouterPath } from '../const';
 import Button from 'react-bootstrap/Button';
 import { Stack } from 'react-bootstrap';
+import { IUsers } from '../Models/IUsers';
+import  {Icon} from 'react-icons-kit';
+import { shoppingCart } from 'react-icons-kit/feather/shoppingCart';
+import { auth } from '../config/Config';
+import { useNavigate } from "react-router-dom";
 
+type NavbarProps = {
+    user: IUsers | null
+}
 
-export function NavBar() {
+export function NavBar({ user }: NavbarProps) {
+    const navigate = useNavigate();
+
+    const HandleLogOut = () => {
+        
+        auth.signOut().then(()=> {
+            navigate(RouterPath.login);
+        })
+    }
+    
     return (
-        <Stack  style={{ marginTop: '20px', padding : '20px 0' , background: 'grey' }}>
+        <Stack style={{ marginTop: '20px', padding: '20px 0', background: 'grey' }}>
             <Container>
-                <Row style={{alignItems: 'center'}}>
+                <Row style={{ alignItems: 'center' }}>
                     <Col className='mb-8'>
                         <div className="main-logo">Logo</div>
                     </Col>
-                    <Col xs={'auto'}>
-                        <Link to={RouterPath.signUp}>
-                            <Button variant="primary" onClick={() => console.log("Primary")}>
-                                SignUp
-                            </Button>
-                        </Link>
-                    </Col>
-                    <Col xs={'auto'}>
-                        <Link to={RouterPath.login}>
-                            <Button variant="outline-dark" onClick={() => console.log("Danger")}>
-                                Login
-                            </Button>
-                        </Link>
-                    </Col>
+                    {user === null ?
+                        (
+                            <Col xs={'auto'}>
+                                <Row>
+                                    <Col>
+                                        <Link to={RouterPath.signUp}>
+                                            <Button variant="primary" onClick={() => console.log("Primary")}>
+                                                SignUp
+                                            </Button>
+                                        </Link>
+                                    </Col>
+                                    <Col >
+                                        <Link to={RouterPath.login}>
+                                            <Button variant="outline-dark" onClick={() => console.log("Danger")}>
+                                                Login
+                                            </Button>
+                                        </Link>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        )  
+                            : (
+                                <Col xs='auto'>
+                                    <Stack direction='horizontal'>
+                                        <div className="user">{user.fullName}</div>
+                                        <Link style={{margin:'0 15px'}} to={'/cart'}>
+                                        <Icon icon={shoppingCart} size={20}/>
+                                        </Link>
+                                        <Button variant="primary" onClick={() => HandleLogOut()}>
+                                                Log out
+                                            </Button>
+
+                                    </Stack>
+                                
+                                </Col>
+                            )
+
+                    }
+                    
+
                 </Row>
             </Container>
         </Stack>
